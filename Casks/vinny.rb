e484afafe8d62eecd1d26ed3e349ad07e3e72aba
sarimabbas <3000809+sarimabbas@users.noreply.github.com>
2026-09-03 09:45:22 -0700

    fix(cask): update Vinny archive checksum

--- Casks/vinny.rb
+++ Casks/vinny.rb
@@ -1,6 +1,6 @@
 cask "vinny" do
   version "0.1.0"
-  sha256 "b108c98de05b6baf2334fdab311c76d1c485137f61387c4e402d7457f69df148"
+  sha256 "874999a14c6dc1c38efab2d37e424cf5ca55f5b5f33f633a9836df076a255f6c"
 
   url "https://github.com/sarimabbas/vinny/releases/download/v#{version}/vinny-#{version}-macos-arm64.zip"
   name "Vinny"
